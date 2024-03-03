1af11492605f7352040ee30857028876098e3020
klyap <kl.yap@hotmail.com>
2024-03-02 16:20:03 -0800

    add shadcn

--- src/app/evaluation/page.tsx
+++ src/app/evaluation/page.tsx
@@ -1,3 +1,32 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 export default function Evaluation() {
-  return <>Evaluation list page</>;
+  return <><h1>Evaluation list page</h1>
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </>;
 }
