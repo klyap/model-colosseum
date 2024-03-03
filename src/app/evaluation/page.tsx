0875ceabc56744d0774715649bd29b61b896831c
klyap <kl.yap@hotmail.com>
2024-03-02 16:51:29 -0800

    basic table

--- src/app/evaluation/page.tsx
+++ src/app/evaluation/page.tsx
@@ -7,26 +7,67 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+
+
+const benchmarks = [
+  "Customer Service Triage Quality Offline ⬇️",
+  "Customer Service Live SBS",
+  "Off-brand responses",
+  "Superfresh Jailbreaks"
+]
+
+interface ModelScores {
+  [key: string]: { version: string };
+}
+const modelsAndScores: ModelScores = {
+  "llama_7b_internal_030224_v2_final": { version: "v3-030224" },
+  "Mixtral 8x7B": { version: "v3-030224" },
+  "Mistral 7B": { version: "v3-030224" },
+  "GPT4": { version: "v3-030224" },
+}
+
 export default function Evaluation() {
-  return <><h1>Evaluation list page</h1>
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+  return <>
+    <div className="m-12">
+      <h1 className="text-xl">March 2 Evaluation Run</h1>
+      <p className="text-slate-500">Benchmarking our latest finetune.</p>
+
+      <Table className="mt-8">
+        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead key={"model-name"}></TableHead>
+
+            {benchmarks.map((benchmarkName) =>
+              <TableHead key={benchmarkName}>{benchmarkName}</TableHead>
+            )}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Object.keys(modelsAndScores).map((model) => (
+            <TableRow key={model}>
+              <TableCell className="font-bold">
+                <div className="flex flex-col">
+                  <div className="">{model}</div>
+                  <div className="text-slate-500 text-xs">{modelsAndScores[model].version}</div>
+                </div>
+              </TableCell>
+              <TableCell className="text-lg">
+                80
+              </TableCell>
+              <TableCell className="text-lg">
+                89
+              </TableCell>
+              <TableCell className="text-lg">
+                71
+              </TableCell>
+              <TableCell className="text-lg">
+                72
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   </>;
 }
