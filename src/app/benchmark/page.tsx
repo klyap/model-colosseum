0500c9495820d8eeb8ec4734f8f38dc7687197a0
Noah MacCallum <noahmacca@gmail.com>
2024-03-02 18:21:40 -0800

    Add test data, hook up filtering buttons

--- src/app/benchmark/page.tsx
+++ src/app/benchmark/page.tsx
@@ -4,9 +4,24 @@ import React from "react";
 import { benchmarks } from "@/lib/data/benchmarks";
 
 export default function Benchmark() {
+  const [dataFilter, setDataFilter] = React.useState("All");
+
+  const benchmark = benchmarks.find((i) => i.id === 123);
+  const data = benchmark?.data.filter(
+    (item) => dataFilter === "All" || item.source === dataFilter
+  );
+
+  const sourceCounts = benchmark?.data.reduce(
+    (acc: { [key: string]: number }, item) => {
+      acc[item.source] = (acc[item.source] || 0) + 1;
+      return acc;
+    },
+    {} as { [key: string]: number }
+  );
+
   return (
     <div className="px-12 my-12">
-      <div className="text-2xl">Customer Service Triage Quality</div>
+      <div className="text-2xl mb-1">Customer Service Triage Quality</div>
       <div className="text-sm">
         <div>
           <span className="font-light">Created by</span>
@@ -19,19 +34,23 @@ export default function Benchmark() {
       </div>
       <div className="py-8">
         <div className="text-lg font-semibold">Description</div>
-        <div className="text-sm text-gray-600 mb-2">
+        <div className="text-sm text-gray-500 mb-2">
           This is used by our synthetic data generator to create samples that
           are as representative and useful as possible. Please provide any
           rubrics and specify the inputs and outputs that you expect.
         </div>
-        <textarea className="w-full p-4 border rounded-md">
-          This benchmark measures the effectiveness and efficiency of customer
+        <textarea
+          className="w-full p-4 border rounded-md"
+          defaultValue="This benchmark measures the effectiveness and efficiency of customer
           service triage systems. It evaluates how well a system can categorize
           and prioritize customer inquiries, ensuring that the most critical
-          issues are addressed promptly and accurately.
-        </textarea>
+          issues are addressed promptly and accurately."
+        />
         <div className="text-right">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded opacity-50"
+            disabled
+          >
             Save
           </button>
         </div>
@@ -39,20 +58,49 @@ export default function Benchmark() {
       <div className="py-8">
         <div className="text-lg font-semibold mb-2">Dataset</div>
         <div className="flex space-x-2 mb-4">
-          <button className="px-2 py-1 text-sm bg-gray-600 text-gray-50 rounded hover:bg-gray-300">
-            All
+          <button
+            onClick={() => setDataFilter("All")}
+            className={`px-2 py-1 text-sm rounded ${
+              dataFilter === "All"
+                ? "bg-gray-500 text-gray-50 hover:bg-gray-600"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            <strong>All</strong> ({benchmark?.data.length || "0"})
           </button>
-          <button className="px-2 py-1 text-sm bg-gray-200 text-gray-700 rounded hover:bg-gray-300">
-            <strong>CSV Upload</strong> (52)
+          <button
+            onClick={() => setDataFilter("csv")}
+            className={`px-2 py-1 text-sm rounded ${
+              dataFilter === "csv"
+                ? "bg-gray-500 text-gray-50 hover:bg-gray-600"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            <strong>CSV Upload</strong> ({sourceCounts?.["csv"] || "0"})
           </button>
-          <button className="px-2 py-1 text-sm bg-gray-200 text-gray-700 rounded hover:bg-gray-300">
-            <strong>Live Users</strong> (612)
+          <button
+            onClick={() => setDataFilter("live")}
+            className={`px-2 py-1 text-sm rounded ${
+              dataFilter === "live"
+                ? "bg-gray-500 text-gray-50 hover:bg-gray-600"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            <strong>Live Users</strong> ({sourceCounts?.["live"] || "0"})
           </button>
-          <button className="px-2 py-1 text-sm bg-gray-200 text-gray-700 rounded hover:bg-gray-300">
-            <strong>Synthetic Data</strong> (10k)
+          <button
+            onClick={() => setDataFilter("synthetic")}
+            className={`px-2 py-1 text-sm rounded ${
+              dataFilter === "synthetic"
+                ? "bg-gray-500 text-gray-50 hover:bg-gray-600"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            <strong>Synthetic Data</strong> (
+            {sourceCounts?.["synthetic"] || "0"})
           </button>
         </div>
-        <table className="min-w-full border-collapse block md:table">
+        <table className="border-collapse block md:table">
           <thead className="block md:table-header-group">
             <tr className="border md:border-none md:table-row">
               <th className="block md:table-cell">Row</th>
@@ -62,35 +110,20 @@ export default function Benchmark() {
             </tr>
           </thead>
           <tbody className="block md:table-row-group">
-            <tr className="border md:border-none md:table-row">
-              <td className="block md:table-cell">1</td>
-              <td className="block md:table-cell">Sample Input 1</td>
-              <td className="block md:table-cell">Sample Output 1</td>
-              <td className="block md:table-cell">System</td>
-            </tr>
-            <tr className="border md:border-none md:table-row">
-              <td className="block md:table-cell">2</td>
-              <td className="block md:table-cell">Sample Input 2</td>
-              <td className="block md:table-cell">Sample Output 2</td>
-              <td className="block md:table-cell">User</td>
-            </tr>
-            <tr className="border md:border-none md:table-row">
-              <td className="block md:table-cell">3</td>
-              <td className="block md:table-cell">Sample Input 3</td>
-              <td className="block md:table-cell">Sample Output 3</td>
-              <td className="block md:table-cell">External</td>
-            </tr>
+            {data?.map((datum, idx) => (
+              <tr key={`key=${idx}`}>
+                <td>{idx}</td>
+                <td>
+                  {datum.input.length > 50
+                    ? `${datum.input.substring(0, 47)}...`
+                    : datum.input}
+                </td>
+                <td>{datum.output}</td>
+                <td>{datum.source}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
-      </div>
-
-      <div className="py-8">
-        <div className="text-lg font-semibold mb-2">DEBUG</div>
-        <ul>
-          {benchmarks.map((benchmark) => (
-            <li key={benchmark.id}>{`Benchmark ID: ${benchmark.id}`}</li>
-          ))}
-        </ul>
       </div>
     </div>
   );
